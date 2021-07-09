2e8262db2db105ee8149e16d25a85d0ceb9f091d
catmario123 <85381172+catmario123@users.noreply.github.com>
2021-07-09 17:57:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,47 @@
-var sea
+var sea, ship;
 var shipmoving;
+var seaImage;
 
 function preload(){
-sea= loadImage ("sea.png")
-shipmoving.loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+seaImage= loadImage ("sea.png")
+shipmoving= loadAnimation("ship-1.png", "ship-1.png", "ship-2.png", "ship-1.png");
 }
 
 function setup(){
   createCanvas(400,400);
   
+//creating sea
+sea=createSprite(400,200);
+sea.addImage(seaImage)
+
+//sea scale
+sea.scale= 0.5;
+
 //creating ship
-ship=createSprite (50,160,50,100);
+ship=createSprite (130,200,30,30);
 ship.addAnimation("moving",shipmoving);
 
 //scale and position of ship
 ship.scale=0.5;
-ship.x=50
 
-//creating sea
-sea=createSprite(200,180,400,20);
-sea.addImage(sea)
 
 }
 
 function draw() {
 
-background("white")
+background (0);
 
   if (sea.x<0){
-    sea.x=sea.width/2;
+    sea.x=sea.width/8;
   }
 
   sea.velocityX=-15;
-  console.log(sea.x);
+  //console.log(sea.x);
+  console.log(ship.x);
  
 //stop the ship from falling
 ship.collide(sea);
+
+drawSprites(); 
 
 }
